47676a5dda1f5f7b00899e6bbb1cb4e5e7dbd106
Julian Papesch <jpapesch000@gmail.com>
2025-08-11 20:07:25 +0200

    Added folders to Blockout folder

--- Assets/_SpaceSafetyVR/_Code/Scripts/Player.cs
+++ Assets/_SpaceSafetyVR/_Code/Scripts/Player.cs
@@ -4,27 +4,8 @@ using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameEvent playerSpawn;
-    [SerializeField] private Interactable playerPos;
     void Awake()
     {
         playerSpawn.Occured();
-
-        if (playerPos == null)
-        {
-            Debug.LogError("No initial PlayerLocation set for Player");
-            return;
-        }
-
-        ChangePlayerLocation(playerPos);
-
-        
-    }
-
-
-    public void ChangePlayerLocation(Interactable newLocation)
-    {
-        if (newLocation == null) return;
-        transform.parent = newLocation.transform;
-        transform.localPosition = Vector3.zero;
     }
 }
