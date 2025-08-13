abe8ed8d419dbb278c13602c65428f53d9e6cbbf
Clyfdir <wuehrerben@clyfdir.com>
2025-08-13 02:43:50 +0200

    Added several Stuff to make the Build possible.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,15 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ATMO = 3133186549U;
+        static const AkUniqueID PLAY_INTRODUCTION_1 = 2632830566U;
         static const AkUniqueID PLAY_LOCKED_DOOR = 281597619U;
+        static const AkUniqueID PLAY_SPACEDEBRIS1 = 2328865502U;
+        static const AkUniqueID PLAY_SPACEDEBRIS2 = 2328865501U;
+        static const AkUniqueID PLAY_SPACEDEBRISEND = 3381056148U;
+        static const AkUniqueID PLAY_SPACEDEBRISEND1 = 3544022733U;
+        static const AkUniqueID PLAY_SPACEDEBRISEND2_01 = 88024092U;
+        static const AkUniqueID PLAY_SPACEDEBRISEND2_02 = 88024095U;
     } // namespace EVENTS
 
     namespace BANKS
