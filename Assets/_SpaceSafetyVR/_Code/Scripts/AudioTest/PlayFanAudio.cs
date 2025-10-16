c1bc9b37559b7dd9d00d6f4f65a5f761445e5a62
Clyfdir <wuehrerben@clyfdir.com>
2025-10-16 21:56:33 +0200

    Fixed some audio bugs. Renamed the Application to Space Safety VR

--- Assets/_SpaceSafetyVR/_Code/Scripts/AudioTest/PlayFanAudio.cs
+++ Assets/_SpaceSafetyVR/_Code/Scripts/AudioTest/PlayFanAudio.cs
@@ -5,8 +5,15 @@ public class PlayFanAudio : MonoBehaviour
 {
 
     public string eventName;
+    public string stopEventName;
     private void Start()
     {
         AudioManager.CallEvent(eventName, this.gameObject);
     }
+
+    private void OnDisable()
+    {
+        AudioManager.CallEvent(stopEventName, this.gameObject);
+    }
 }
+
